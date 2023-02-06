01c7285635332a509786059ab295e781705f6e6a
Kiny <89192734912nik@gmail.com>
2023-02-07 00:01:48 +0300

    init

---
+++ Milestone 10-12/Milestone 10-12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone 10-12
+//
+//  Created by nikita on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
