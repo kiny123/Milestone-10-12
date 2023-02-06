50baf6a8ac082707563e36fed9515bbd50a8da58
Kiny <89192734912nik@gmail.com>
2023-02-07 00:53:40 +0300

    end

---
+++ Milestone 10-12/Milestone 10-12/PictureCell.swift
@@ -0,0 +1,15 @@
+//
+//  PictureCell.swift
+//  Milestone 10-12
+//
+//  Created by nikita on 07.02.2023.
+//
+
+import UIKit
+
+class PictureCell: UITableViewCell {
+    @IBOutlet var pictureImage: UIImageView!
+    @IBOutlet var labelImage: UILabel!
+    
+    
+}
